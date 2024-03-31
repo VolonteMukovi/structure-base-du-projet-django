86b5e2e4b7b2727c1d5c164559154a239e611cfe
volonteMukovi <mukovivolonte@gmail.com>
2024-03-31 15:29:39 +0200

    cree mes propres urls

--- Essai1/settings.py
+++ Essai1/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Essai1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["Templetes"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
