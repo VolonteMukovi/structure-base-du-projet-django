ad38a90fc91b64e9bb95c637ab257d9e2fcdaf10
volonteMukovi <mukovivolonte@gmail.com>
2024-03-31 16:15:30 +0200

    creation des templete html et decouverte de la foction render

--- Essai1/views.py
+++ Essai1/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse("Hello world")
+    return render(request,"home.html")
+    # return HttpResponse("Hello world")
 
 def contact_view(request):
-    return HttpResponse("Contactez nous ! ")
+    return render(request,"contact.html")
+    # return HttpResponse("Contactez nous ! ")
